cef6754820e8eae2215ccef83388c0f1b26909b4
Keshavcodes3 <Keshavcodes0@gmail.com>
2026-07-30 21:03:07 +0530

    fix and merge:fixing routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,12 +10,12 @@ import authRouter from "@/modules/auth/auth.routes.js"
 import courseRouter from "@/modules/courses/courses.routes.js"
 import { ApiError } from "@/middlewares/apiError.js"
 import moduleRouter from "@/modules/course-module/course-module.routes.js"
-
+import lessonRouter from "@/modules/lessons/lessons.routes.js"
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/courses', courseRouter)
-app.use('/api/v1/modules',moduleRouter)
-
+app.use('/api/v1/modules', moduleRouter)
+app.use('/api/v1/lessons', lessonRouter)
 
 app.use((err: Error, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
     if (err instanceof ApiError) {
